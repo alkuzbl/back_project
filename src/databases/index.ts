c07029a527e30fb32db3834a03be34f7d5a6d636
alkuzbl <84181803+alkuzbl@users.noreply.github.com>
2022-02-19 00:54:15 +0300

    Fixed: plainToClass replaced plainToInstance

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,7 +1,8 @@
-import { DB_HOST, DB_PORT, DB_DATABASE } from '@config';
+import { DB_USER, DB_PASSWORD, DB_DATABASE } from '@config';
 
 export const dbConnection = {
-  url: `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
+  // url: `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
+  url: `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.cwd26.mongodb.net/${DB_DATABASE}?retryWrites=true&w=majority`,
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
